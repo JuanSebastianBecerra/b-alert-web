e724ec080f9149bdafd23cb7df7eab3e9faf8be4
Nestor Pérez Espinel <neztoring@gmail.com>
2023-09-27 11:47:41 -0500

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreateConfigurationComponent } from './create-configuration/create-configuration.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'create-configuration', component: CreateConfigurationComponent },
   { path: 'landingpage', component: LandingpageComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CreateConfigurationComponent } from './create-configuration/create-configuration.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CreateConfigurationComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
